b6e343d1d8398eaa67da66fce1a1e44bc30be332
hasansezertasan <hasansezertasan@gmail.com>
2023-12-30 10:11:03 +0300

    Final touches before the initial release.

--- src/starlette_flask/middleware/sessions.py
+++ src/starlette_flask/middleware/sessions.py
@@ -1,7 +1,5 @@
-import sys
 import typing
 from hashlib import sha1
-from typing import Union
 
 from itsdangerous import BadSignature, URLSafeTimedSerializer
 from starlette.datastructures import MutableHeaders, Secret
@@ -17,8 +15,9 @@ class SessionMiddleware:
         session_cookie: str = "session",
         max_age: typing.Optional[int] = 14 * 24 * 60 * 60,  # 14 days, in seconds
         path: str = "/",
-        same_site: Literal["lax", "strict", "none"] = "lax",
+        same_site: typing.Literal["lax", "strict", "none"] = "lax",
         https_only: bool = False,
+        domain: typing.Optional[str] = None,
         salt: bytes = b"cookie-session",
         signer_kwargs: dict = {
             "key_derivation": "hmac",
@@ -30,13 +29,15 @@ class SessionMiddleware:
         self.max_age = max_age
         self.path = path
         self.security_flags = "httponly; samesite=" + same_site
+        if https_only:  # Secure flag can be used with HTTPS only
+            self.security_flags += "; secure"
+        if domain is not None:
+            self.security_flags += f"; domain={domain}"
         self.serializer = URLSafeTimedSerializer(
             secret_key=secret_key if isinstance(secret_key, bytes) else secret_key.encode("utf-8"),
             salt=salt,
             signer_kwargs=signer_kwargs,
         )
-        if https_only:  # Secure flag can be used with HTTPS only
-            self.security_flags += "; secure"
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if scope["type"] not in ("http", "websocket"):  # pragma: no cover
@@ -44,7 +45,6 @@ class SessionMiddleware:
             return
 
         connection = HTTPConnection(scope)
-        initial_session_was_empty = True
         session_initial = {}
 
         if self.session_cookie in connection.cookies:
@@ -53,7 +53,6 @@ class SessionMiddleware:
                 session = self.serializer.loads(data)
                 session_initial = session.copy()
                 scope["session"] = session
-                initial_session_was_empty = False
             except BadSignature:
                 scope["session"] = {}
         else:
@@ -61,10 +60,23 @@ class SessionMiddleware:
 
         async def send_wrapper(message: Message) -> None:
             if message["type"] == "http.response.start":
-                if scope["session"] and scope["session"] == session_initial:
-                    ...
-                    # Session data is not changed.
-                elif scope["session"] and scope["session"] != session_initial:
+                headers = MutableHeaders(scope=message)
+                if not scope["session"]:
+                    print("Session is empty!")
+                    # The session has been cleared.
+                    headers = MutableHeaders(scope=message)
+                    header_value = (
+                        "{session_cookie}={data}; path={path}; {expires}{security_flags}".format(  # noqa E501
+                            session_cookie=self.session_cookie,
+                            data="null",
+                            path=self.path,
+                            expires="expires=Thu, 01 Jan 1970 00:00:00 GMT; ",
+                            security_flags=self.security_flags,
+                        )
+                    )
+                    headers.append("Set-Cookie", header_value)
+                elif scope["session"] != session_initial:
+                    print("Session is changed!")
                     # Session data is changed - We have session data to persist.
                     data = self.serializer.dumps(scope["session"])
                     headers = MutableHeaders(scope=message)
@@ -78,19 +90,8 @@ class SessionMiddleware:
                         )
                     )
                     headers.append("Set-Cookie", header_value)
-                elif not initial_session_was_empty:
-                    # The session has been cleared.
-                    headers = MutableHeaders(scope=message)
-                    header_value = (
-                        "{session_cookie}={data}; path={path}; {expires}{security_flags}".format(  # noqa E501
-                            session_cookie=self.session_cookie,
-                            data="null",
-                            path=self.path,
-                            expires="expires=Thu, 01 Jan 1970 00:00:00 GMT; ",
-                            security_flags=self.security_flags,
-                        )
-                    )
-                    headers.append("Set-Cookie", header_value)
+                else:
+                    print("Session is not changed!")
             await send(message)
 
         await self.app(scope, receive, send_wrapper)
